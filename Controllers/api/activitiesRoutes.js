c93e0453fb5fb48ac7231b44837fcafed5aa67f8
James <joshua.james918@gmail.com>
2023-09-09 17:42:41 -0500

    activities, trips, user, routes

---
+++ Controllers/api/activitiesRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Activities } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newActivities = await Activities.create({
+      ...req.body,
+      trip_id: req.session.trip_id,
+    });
+
+    res.status(200).json(newActivities);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const activitiesData = await Activities.destroy({
+      where: {
+        id: req.params.id,
+        trip_id: req.session.trip_id,
+      },
+    });
+
+    if (!activitiesData) {
+      res.status(404).json({ message: 'No activity found !' });
+      return;
+    }
+
+    res.status(200).json(activitiesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./tripsRoute');
+const projectRoutes = require('./projectRoute');
+const activitiesRoutes = require('./activitiesRoutes');
+const tripsRoutes = require('./tripsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/Trips', tripsRoutes);
+router.use('/activitiesRoutes', activitiesRoutes);
 
 module.exports = router;
