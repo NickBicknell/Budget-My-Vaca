7ad214aa38eb1c2ba9cf82006cd8a9587689161b
Nick Bicknell <nicholasbicknell@gmail.com>
2023-09-11 19:29:19 -0500

    configured modex index.js exports

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const Trip = require('./Trip');
 const Activities = require('./Activities');
+const User = require('./User');
+const Project = require('./Project');
 
 Trip.hasMany(Activities, {
   foreignKey: 'trip_id',
@@ -10,4 +12,18 @@ Activities.belongsTo(Trip, {
   foreignKey: 'trip_id'
 });
 
-module.exports = { Trip, Activities };
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { Trip, Activities, User, Project };
+
+
+
+
+
