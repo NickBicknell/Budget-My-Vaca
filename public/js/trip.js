acfcbaf78df7c36518060794bd7b2aaeb5c19005
Nick Bicknell <nicholasbicknell@gmail.com>
2023-09-13 18:28:50 -0500

    added function to add a new vacay to the trip table in trip_db

--- public/js/trip.js
+++ public/js/trip.js
@@ -1,5 +1,6 @@
-`<h1 class='fs-2 mb-5'>Spending Money: {{trip_budget}}</h1>
-
+`<h1 class='fs-2 mb-3'>Spending Money: {{trip_budget}}</h1>
+<button type='button' class='btn btn-primary mb-4'><a class='text-white link-underline-primary' id='new-activity-btn'
+    href='/'>New Activity</a></button>
 <div class='row mb-5'>
   <div class='col-2'>
     <h1 class='fs-4 text-decoration-underline'>Days</h1>
@@ -12,11 +13,33 @@
   <div class='col-2'>
     <h1 class='fs-5'>Day 1</h1>
   </div>
-  <div class="col-10">
-      <button type='button' class='btn btn-primary'><a
-          class='text-white link-underline-primary'
-          id='new-activity-btn'
-          href='/'
-        >New Activity</a></button>
+  <div class='col-10'>
+    -- Activities will go here --
   </div>
 </div>`
+
+const newVacayHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#vacay-name').value.trim();
+    const days = document.querySelector('#trip-days').value.trim();
+    const budget = document.querySelector('#start-budget').value.trim();
+  
+    if (name && days && budget) {
+      const response = await fetch('/api/trips', {
+        method: 'POST',
+        body: JSON.stringify({ name, days: parseInt(days), budget: parseInt(budget) }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/currentVacay');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('.new-vacay-form')
+  .addEventListener('submit', newVacayHandler);
